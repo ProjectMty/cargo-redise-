9bcffc60b124743095015d7bed34a734d31938b9
ailin-cantu <it03@cargomty.com>
2025-10-10 17:23:17 -0600

    Pasos y tarifas
    -Pasos -> Diseño completo, animaciones por revisar
    -Tarifas -> inicio de diseño(falta svg de iconos)

--- src/components/ComoFunciona.tsx
+++ src/components/ComoFunciona.tsx
@@ -4,6 +4,7 @@ import { motion, easeOut } from "framer-motion";
 import Image from "next/image";
 import { useIsMobile } from "@/hooks/useIsMobile";
 import "@/style/ComoFunciona.css";
+import AnimatedText from "@/animate/TextAnimate";
 
 // #region animation
 const ContainerSteps = {
@@ -17,9 +18,9 @@ const ContainerSteps = {
 };
 
 const StepsAnimationUp = {
-  hidden: { opacity: 0, y: 40 },
+  hidden: { y: 40 },
   show: {
-    opacity: 1,
+
     y: 0,
     transition: {
       duration: 1,
@@ -29,7 +30,7 @@ const StepsAnimationUp = {
 };
 
 const StepsAnimationDown = {
-  hidden: { opacity: 0, y: -40 },
+  hidden: { opacity: 1, y: -40 },
   show: {
     opacity: 1,
     y: 0,
@@ -59,39 +60,27 @@ export default function ComoFunciona() {
       <div className="div-titulo">
         {/* Título */}
         <h2 className="title">
-          Solo tienes que seguir estos sencillos pasos
+          <AnimatedText 
+          delay={0.2}
+          lines={["Solo tienes que seguir estos sencillos pasos"]}
+          >
+          </AnimatedText>
+          
         </h2>
 
         {/* Contenedor principal con fondo blanco */}
         <div className="contenedor-principal">
-
-          {/* Línea horizontal */}
-          <div className="contenedor-central">
-            <div className="cuadrado"></div>
-            <div className="linea-Vertical"></div>
-            <div className="cuadrado"></div>
-            <div className="cuadrado"></div>
-            <div className="linea-Vertical"></div>
-            <div className="cuadrado"></div>
-            <div className="linea-Vertical"></div>
-            <div className="cuadrado"></div>
-            <div className="linea-Vertical"></div>
-            <div className="cuadrado"></div>
-            <div className="linea-Vertical"></div>
-            <div className="cuadrado"></div>
-            <div className="linea-Vertical"></div>
-            <div className="cuadrado"></div>
-
-          </div>
-
-
+          <div className="cuadrado-arriba"></div>
+          <div className="linea-Vertical mt-16"></div>
           {/* Grid de pasos */}
-          <div className="grid-pasos">
+          <div className="grid-pasos mt-36">
+
 
             {/* Paso 1 izquierda*/}
             <motion.div
               variants={isMobile ? StepsAnimationDown : StepsAnimationUp}
-              className="contenedor-paso">
+              className="contenedor-paso-izquierdo">
+
               <div className="imagen-paso-1">
                 <Image
                   src="/img/funciona/Paso-1.png"
@@ -99,13 +88,16 @@ export default function ComoFunciona() {
                   width={250}
                   height={250} />
               </div>
-              <div className="contenedor-paso-titulo">
+              <div className="contenedor-paso-titulo-izquierdo">
                 <h2>Realiza tus compras</h2>
+                <div className="cuadrado-izquierda"></div>
+                <div className="linea-horizontal-izquierda"></div>
               </div>
-              <p className="contenedor-paso-texto">
+              <p className="contenedor-paso-texto-izquierdo -ml-6">
                 Compra en línea con tu proveedor o en
                 páginas reconocidas
               </p>
+
             </motion.div>
 
             {/* separacion de grid para pasos */}
@@ -120,11 +112,13 @@ export default function ComoFunciona() {
                 <Image
                   src="/img/funciona/Paso-2.png"
                   alt="paso 2"
-                  width={250}
-                  height={250} />
+                  width={350}
+                  height={350} />
               </div>
               <div className="contenedor-paso-titulo-derecho">
                 <h2>Dirección en Laredo, TX</h2>
+                <div className="cuadrado-derecha"></div>
+                <div className="linea-horizontal-derecha"></div>
               </div>
               <p className="contenedor-paso-texto-derecho">
                 Asigna la dirección de nuestro almacén en Estados Unidos
@@ -135,18 +129,20 @@ export default function ComoFunciona() {
             {/* Paso 3 izquierda*/}
             <motion.div
               variants={isMobile ? StepsAnimationDown : StepsAnimationUp}
-              className="contenedor-paso">
+              className="contenedor-paso-izquierdo">
               <div className="imagen-paso-3">
                 <Image
                   src="/img/funciona/Paso-3.png"
                   alt="paso 3"
-                  width={250}
-                  height={250} />
+                  width={350}
+                  height={350} />
               </div>
-              <div className="contenedor-paso-titulo">
+              <div className="contenedor-paso-titulo-izquierdo">
                 <h2>Comparte tus comprobantes</h2>
+                <div className="cuadrado-izquierda"></div>
+                <div className="linea-horizontal-izquierda mt-3"></div>
               </div>
-              <p className="contenedor-paso-texto">
+              <p className="contenedor-paso-texto-izquierdo">
                 Envía a tu asesor tu pago y número de rastreo
               </p>
             </motion.div>
@@ -164,11 +160,13 @@ export default function ComoFunciona() {
                 <Image
                   src="/img/funciona/Paso-4.png"
                   alt="paso 4"
-                  width={250}
-                  height={250} />
+                  width={400}
+                  height={400} />
               </div>
               <div className="contenedor-paso-titulo-derecho">
                 <h2 className="px-5">Trámite aduanal</h2>
+                <div className="cuadrado-derecha"></div>
+                <div className="linea-horizontal-derecha"></div>
               </div>
               <p className="contenedor-paso-texto-derecho">
                 Nosotros nos encargamos de cruzar tu mercancía a México
@@ -179,45 +177,55 @@ export default function ComoFunciona() {
             {/* Paso 5 izquierda*/}
             <motion.div
               variants={isMobile ? StepsAnimationDown : StepsAnimationUp}
-              className="contenedor-paso">
+              className="contenedor-paso-izquierdo">
               <div className="imagen-paso-5">
                 <Image
                   src="/img/funciona/Paso-5.png"
                   alt="paso 3"
-                  width={250}
-                  height={250} />
+                  width={300}
+                  height={300} />
               </div>
-              <div className="contenedor-paso-titulo">
+              <div className="contenedor-paso-titulo-izquierdo">
                 <h2>Pago y entrega</h2>
+                <div className="cuadrado-izquierda"></div>
+                <div className="linea-horizontal-izquierda"></div>
               </div>
-              <p className="contenedor-paso-texto">
+              <p className="contenedor-paso-texto-izquierdo">
                 Pagas honorarios e impuestos, y recibes en tu domicilio
               </p>
             </motion.div>
 
-
-          </div>
-
-          <div className="contenedor-central">
-
+            {/* Paso 6 abajo-central */}
             <motion.div
               variants={isMobile ? StepsAnimationDown : StepsAnimationUp}
-              className="contenedor-paso mt-36">
-              <div className="imagen-paso-6">
-                <Image
-                  src="/img/funciona/Paso-6.png"
-                  alt="paso 6"
-                  width={250}
-                  height={250} />
+              className="contenedor-paso-central col-span-2">
+              <div>
+
+
+                <div className="imagen-paso-6">
+                  <Image
+                    src="/img/funciona/Paso-6.png"
+                    alt="paso 6"
+                    width={250}
+                    height={250} />
+                </div>
+                <div className="contenedor-paso-titulo-central">
+                  <h2>Facturación</h2>
+                  <div className="cuadrado-abajo"></div>
+                </div>
+                <p className="contenedor-paso-texto-central">
+                  Te generamos factura por servicios o productos
+
+                </p>
               </div>
-              <div className="contenedor-paso-titulo">
-                <h2>Facturación</h2>
-              </div>
-              <p className="contenedor-paso-texto-central">
-                Te generamos factura por servicios o productos
-              </p>
             </motion.div>
+
           </div>
+
+
+
+
+
 
         </div>
 
--- src/components/Tarifas.tsx
+++ src/components/Tarifas.tsx
@@ -3,97 +3,119 @@
 
 import AnimatedText from "@/animate/TextAnimate";
 import DivZoom from "@/animate/DivZoom";
+import "@/style/Tarifas.css"
 
 export default function Tarifas() {
   return (
-    <section className="w-full bg-white py-16 scroll mt-15 font-[Montserrat]" id="tarifas">
-      <div className="max-w-7xl mx-auto px-4 md:px-8">
+    <section className="section" id="tarifas">
+      <div className="contenedor">
 
-        <AnimatedText
-          delay={0.2}
-          lines={[
-            < h2 key={1} className="text-center text-blue-800 text-4xl md:text-5xl font-bold mb-12">
-              Tarifas
-            </h2>
-          ]}>
-        </AnimatedText>
+        <div className="contenedor-titulo ">
 
 
+          <AnimatedText
+            delay={0.2}
+            lines={[
+              < h2 key={1}>
+                Tarifas
+              </h2>
+            ]}>
+          </AnimatedText>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        </div>
+
+        <div className="contenedor-tarifas">
           {/* Tarjeta 1 */}
-          <div className="bg-[#0072BC] text-white rounded-2xl shadow-lg p-6 transition-transform duration-300 hover:-translate-y-2">
-            <h3 className="text-center font-bold text-lg">COMPRAS PERSONALES</h3>
-            <p className="text-center font-semibold mb-4">(Menor a $119 USD)</p>
-            <hr className="border-white/50 mb-4" />
-            <p className="text-center font-bold text-xl mb-4">Tarifa de $300 MXN</p>
-            <p className="text-xs mb-4">Si la compra es menor a $100 USD, pero es mercancía repetitiva o para vender, adicional al servicio de honorario, se cobra el 16% IVA.</p>
-            <p className="text-xs">*Sobrepeso: Arriba de 20 kg peso volumétrico se le agrega concepto de sobrepeso.</p>
+          <div className="tarjeta-tarifas">
+            <h3 className="titulo-tarifas">COMPRAS PERSONALES</h3>
+            <p className="subtitulo-tarifas">(Menor a $119 USD)</p>
+
           </div>
 
           {/* Tarjeta 2 */}
-          <div className="bg-[#0072BC] text-white rounded-2xl shadow-lg p-6 transition-transform duration-300 hover:-translate-y-2">
-            <h3 className="text-center font-bold text-lg">COMPRAS PERSONALES</h3>
-            <p className="text-center font-semibold mb-4">($120 A $475 USD)</p>
-            <hr className="border-white/50 mb-4" />
-            <p className="text-center font-bold text-xl mb-4">14% sobre el valor de tu compra</p>
-            <p className="text-xs mb-4">Si la compra es menor a $475 USD, pero es mercancía repetitiva o para vender, adicional al servicio de honorario, se cobra el 16% IVA.</p>
-            <p className="text-xs">*Sobrepeso: Arriba de 20 kg peso volumétrico se le agrega concepto de sobrepeso.</p>
+          <div className="tarjeta-tarifas">
+            <h3 className="titulo-tarifas">COMPRAS PERSONALES</h3>
+            <p className="subtitulo-tarifas">($120 A $475 USD)</p>
           </div>
 
           {/* Tarjeta 3 */}
-          <div className="bg-[#0072BC] text-white rounded-2xl shadow-lg p-6 transition-transform duration-300 hover:-translate-y-2">
-            <h3 className="text-center font-bold text-lg">COMPRAS PERSONALES</h3>
-            <p className="text-center font-semibold mb-4">($476 a $3000 USD)</p>
-            <hr className="border-white/50 mb-4" />
-            <p className="text-center font-bold text-xl mb-4">13% sobre el valor de tu compra</p>
-            <p className="text-xs mb-4">Si la compra es menor a $3,000 USD, pero es mercancía repetitiva o para vender, adicional al servicio de honorario, se cobra el 16% IVA.</p>
-            <p className="text-xs">*Sobrepeso: Arriba de 20 kg peso volumétrico se le agrega concepto de sobrepeso.</p>
+          <div className="tarjeta-tarifas">
+            <h3 className="titulo-tarifas">COMPRAS PERSONALES</h3>
+            <p className="subtitulo-tarifas">($476 a $3000 USD)</p>
           </div>
 
           {/* Tarjeta 4 */}
-          <div className="bg-[#0072BC] text-white rounded-2xl shadow-lg p-6 transition-transform duration-300 hover:-translate-y-2">
-            <h3 className="text-center font-bold text-lg">EMPRESARIALES</h3>
-            <p className="text-center font-semibold mb-4">A partir de $3000 USD</p>
-            <hr className="border-white/50 mb-4" />
-            <p className="text-center font-sm mb-4">Las compras empresariales se cotizan de acuerdo con la naturaleza del producto, para calcular los aranceles y los fletes, los cuales dependen del peso y volumen de la mercancía.</p>
-            <p className="text-center font-bold text-lg mb-4">12% sobre el valor de tu compra</p>
-            <p className="text-xs">*Sobrepeso: Arriba de 20 kg peso volumétrico se le agrega concepto de sobrepeso.</p>
+          <div className="tarjeta-tarifas">
+            <h3 className="titulo-tarifas">COMPRAS EMPRESARIALES</h3>
+            <p className="subtitulo-tarifas">(A partir de $3000 USD)</p>
+          </div>
+        </div>
+
+        <div className="contenedor-tarifas-informacion">
+          {/* Tarjeta 1 */}
+          <div className="tarjeta-tarifas">
+            <h3 className="titulo-tarifas">COMPRAS PERSONALES</h3>
+            <p className="subtitulo-tarifas">(Menor a $119 USD)</p>
+            <p className="subtitulo-tarifas">Costo del servicio</p>
+           <p className="precio-tarifa">$300 MXN</p>
+
+            <p className="texto-tarifa">Si la compra es menor a $100 USD, pero es mercancía repetitiva o para vender, adicional al servicio de honorario, se cobra el 16% IVA.</p>
+           <p className="texto-tarifa">*Sobrepeso: Arriba de 20 kg peso volumétrico se le agrega concepto de sobrepeso.</p>
+
+
+          </div>
+
+          {/* Tarjeta 2 */}
+          <div className="tarjeta-tarifas">
+            <h3 className="titulo-tarifas">COMPRAS PERSONALES</h3>
+            <p className="subtitulo-tarifas">($120 A $475 USD)</p>
+          </div>
+
+          {/* Tarjeta 3 */}
+          <div className="tarjeta-tarifas">
+            <h3 className="titulo-tarifas">COMPRAS PERSONALES</h3>
+            <p className="subtitulo-tarifas">($476 a $3000 USD)</p>
+          </div>
+
+          {/* Tarjeta 4 */}
+          <div className="tarjeta-tarifas">
+            <h3 className="titulo-tarifas">COMPRAS EMPRESARIALES</h3>
+            <p className="subtitulo-tarifas">(A partir de $3000 USD)</p>
           </div>
         </div>
       </div>
       {/* Cobros Adicionales */}
 
-       
+
       <div className="mt-16 flex justify-center " >
         <DivZoom scale={1.05}>
-        <div className="bg-[#ffffff] rounded-xl shadow-md p-6 md:p-8 max-w-4xl w-full">
-          <h3 className="text-xl md:text-2xl font-bold text-color text-blue-800 mb-4 text-center">
-            Cobros adicionales
-          </h3>
-          <ul className="space-y-3 text-sm md:text-base text-[#333333]">
-            <li>
-              • El envío al destino final en México no está incluido y será
-              determinado por separado, según el destino y las características
-              del paquete.
-            </li>
-            <li>
-              <span className="font-bold">$3.00 USD</span> por cada bolsa, caja o
-              sobre recibido.
-            </li>
-            <li>
-              <span className="font-bold">$10.00 USD</span> por cada tarima descargada.
-            </li>
-            <li>
-              <span className="font-bold">$375.00 USD</span> por cada tarima recibida
-              de hasta 500 libras.
-            </li>
-            <li>
-              <span className="font-bold">$515.00 USD</span> por cada tarima recibida
-              que supere las 500 libras.
-            </li>
-          </ul>
-        </div>
+          <div className="bg-[#ffffff] rounded-xl shadow-md p-6 md:p-8 max-w-4xl w-full">
+            <h3 className="text-xl md:text-2xl font-bold text-color text-blue-800 mb-4 text-center">
+              Cobros adicionales
+            </h3>
+            <ul className="space-y-3 text-sm md:text-base text-[#333333]">
+              <li>
+                • El envío al destino final en México no está incluido y será
+                determinado por separado, según el destino y las características
+                del paquete.
+              </li>
+              <li>
+                <span className="font-bold">$3.00 USD</span> por cada bolsa, caja o
+                sobre recibido.
+              </li>
+              <li>
+                <span className="font-bold">$10.00 USD</span> por cada tarima descargada.
+              </li>
+              <li>
+                <span className="font-bold">$375.00 USD</span> por cada tarima recibida
+                de hasta 500 libras.
+              </li>
+              <li>
+                <span className="font-bold">$515.00 USD</span> por cada tarima recibida
+                que supere las 500 libras.
+              </li>
+            </ul>
+          </div>
         </DivZoom>
       </div>
 
