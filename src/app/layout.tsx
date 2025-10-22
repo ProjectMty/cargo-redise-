0d376c92ce9448487fcfdf64d4a51dbb32d85cb8
ailin-cantu <it03@cargomty.com>
2025-10-22 13:28:16 -0600

    update errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { CalculadoraVisibleProvider } from "@/context/CalculadoraVisibleContext";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,13 +16,30 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-    return (
-      <html lang="es" className="scroll-smooth">
-        <body className={inter.className + "  text-gray-900"}>
-          <CalculadoraVisibleProvider>
-            <main className="pt-0">{children}</main>
-          </CalculadoraVisibleProvider>
-        </body>
-      </html>
-    );
+  return (
+    <html lang="es" className="scroll-smooth">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-969438638"
+        />
+        <Script id="google-ads-script" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-969438638');
+          `}
+        </Script>
+      </head>
+      <body className={inter.className + "  text-gray-900"}>
+        <script>
+          {`  gtag('event', 'conversion', {'send_to': 'AW-969438638/7EDlCIb1xYACEK7roc4D'}); `}
+        </script>
+        <CalculadoraVisibleProvider>
+          <main className="pt-0">{children}</main>
+        </CalculadoraVisibleProvider>
+      </body>
+    </html>
+  );
 }
